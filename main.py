8b2be45707aa4622853954116676d6c2d2c9e7b7
AterLi <132461103+AterLi@users.noreply.github.com>
2024-07-11 15:19:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+data = requests.get(
+    url="https://web.archive.org/web/20200518055830/https://www.empireonline.com/movies/features/best-movies-2/")
+web_data = data.text
+
+soup = BeautifulSoup(web_data, "html.parser")
+
+all_titles = soup.find_all("h3", class_="title")
+for i in all_titles[::-1]:
+    movie = i.getText()
+    with open(file="movies.txt", mode="a",  encoding='utf-8') as file:
+        file.write(f"{movie}\n")
+        print(movie)
+
+# for i in all_titles:
+#     with open(file="./movie_list.txt", mode="a") as to_watch:
+#         title = i.getText()
+#         to_watch.write(f"{title}\n")
+
+
+
+
